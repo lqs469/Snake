729f4578265a31643535a074deb2a7a6cf0befc7
lqs469 <liqinshuo469@gmail.com>
2017-03-22 00:32:08 +0800

    level

--- main.js
+++ main.js
@@ -1,150 +1,146 @@
 var root = document.getElementById('root')
+
 const step = 10
-const __LEFT__ = '__LEFT__'
-const __UP__ = '__TOP__'
-const __RIGHT__ = '__RIGHT__'
+const __UP__ = '__UP__'
 const __DOWN__ = '__DOWN__'
+const __LEFT__ = '__LEFT__'
+const __RIGHT__ = '__RIGHT__'
 
 var h = new tail(24, 24)
 var snake = [h]
-var apple = new apple(Math.round(Math.random() * 49), Math.round(Math.random() * 49))
+var level = 1
 
 function tail (x, y) {
   this.x = x
   this.y = y
-  this.dir = ''
+  this.dir
 
-  this.tail = document.createElement('div')
-  this.tail.className = 'tail'
-
-  this.tail.style.left = `${this.x * step}px`
-  this.tail.style.top = `${this.y * step}px`
-  root.appendChild(this.tail)
+  this.body = document.createElement('div')
+  this.body.className = 'body'
+  this.body.style.left = `${x * 10}px`
+  this.body.style.top = `${y * 10}px`
+  root.appendChild(this.body)
 
   this.move = (dir) => {
-    this.dir = dir || this.dir
+    this.dir = dir
     this.eat()
 
-    var overSize = (value) => value > 49 || value < 0 ? this.death() : value
-
-    switch (this.dir) {
+    switch (dir) {
       case __UP__:
-        this.y = overSize(this.y - 1)
+        this.y--
         break
       case __DOWN__:
-        this.y = overSize(this.y + 1)
+        this.y++
         break
       case __LEFT__:
-        this.x = overSize(this.x - 1)
+        this.x--
         break
       case __RIGHT__:
-        this.x = overSize(this.x + 1)
+        this.x++
+        break
       default:
     }
+    this.body.style.left = `${this.x * 10}px`
+    this.body.style.top = `${this.y * 10}px`
 
-    this.tail.style.left = `${this.x * step}px`
-    this.tail.style.top = `${this.y * step}px`
+    if (this.x > 49 || this.x < 0 || this.y > 49 || this.y < 0) {
+      this.dead()
+    }
+
     return this
   }
 
+  this.follow = () => {
+    for (let i = snake.length - 1; i > 0; i--) {
+      snake[i].move(snake[i].dir)
+      snake[i].dir = snake[i - 1].dir
+    }
+  }
+
   this.eat = () => {
-    if (this.x === apple.x && this.y === apple.y) {
-      apple.reborn()
+    snake.map((b, i) => {
+      if (i > 0 && b.x === h.x && b.y === h.y) {
+        this.dead()
+      }
+    })
+
+    if (this.x === apple.x && this.y == apple.y) {
       this.grow()
+      apple.reborn()
+      level++
     }
   }
 
   this.grow = () => {
-    let last = snake[snake.length - 1]
     let t
+    let last = snake[snake.length - 1]
 
-    switch (snake[snake.length - 1].dir) {
-      case __DOWN__:
-        t = new tail(last.x, last.y - 1)
-      break
+    switch (last.dir) {
       case __UP__:
         t = new tail(last.x, last.y + 1)
-      break
-      case __RIGHT__:
-        t = new tail(last.x - 1, last.y)
-      break
+        break
+      case __DOWN__:
+        t = new tail(last.x, last.y - 1)
+        break
       case __LEFT__:
         t = new tail(last.x + 1, last.y)
-      break
+        break
+      case __RIGHT__:
+        t = new tail(last.x - 1, last.y)
+        break
       default:
     }
 
     t.dir = last.dir
     snake.push(t)
-
-    console.log(`points ${snake.length}`)
   }
 
-  this.follow = () => {
-    for (let i = snake.length - 1; i > 0; i--) {
-      snake[i].move().dir = snake[i - 1].dir
-    }
-
-    return this
-  }
-
-  this.self = () => {
-    snake.map((t, i) => {
-      if (i > 0 && this.x === t.x && this.y === t.y) {
-        this.death()
-      }
-    })
-  }
-
-  this.death = () => {
-    alert('game over!')
+  this.dead = () => {
+    alert(`game over!!\npoints: ${snake.length - 1}`)
     location.reload()
   }
 }
 
+var apple = new apple(Math.round(Math.random() * 49), Math.round(Math.random() * 49))
+
 function apple (x, y) {
   this.x = x
   this.y = y
+  this.dir
 
-  this.apple = document.createElement('div')
-  this.apple.className = 'apple'
-
-  this.apple.style.left = `${x * step}px`
-  this.apple.style.top = `${y * step}px`
-  root.appendChild(this.apple)
+  this.body = document.createElement('div')
+  this.body.className = 'apple'
+  this.body.style.left = `${x * 10}px`
+  this.body.style.top = `${y * 10}px`
+  root.appendChild(this.body)
 
   this.reborn = () => {
     this.x = Math.round(Math.random() * 49)
     this.y = Math.round(Math.random() * 49)
-    this.apple.style.left = `${this.x * step}px`
-    this.apple.style.top = `${this.y * step}px`
+    this.body.style.left = `${this.x * 10}px`
+    this.body.style.top = `${this.y * 10}px`
   }
 }
 
 var interval
 
 window.addEventListener('keydown', (e) => {
-  if (!h.dir || (h.dir === __RIGHT__ && e.key != 'a') ||
-    (h.dir === __LEFT__ && e.key != 'd') ||
-    (h.dir === __UP__ && e.key != 's') ||
-    (h.dir === __DOWN__ && e.key != 'w')) {
-    clearInterval(interval)
-    interval = window.setInterval(() => {
-      switch (e.key) {
-        case 'w':
-          h.move(__UP__).follow().self()
-          break
-        case 's':
-          h.move(__DOWN__).follow().self()
-          break
-        case 'a':
-          h.move(__LEFT__).follow().self()
-          break
-        case 'd':
-          h.move(__RIGHT__).follow().self()
-          break
-        default:
-      }
-    }, 20)
-  }
+  clearInterval(interval)
+  interval = window.setInterval(() => {
+    switch (e.key) {
+      case 'w':
+        h.dir !== __DOWN__ ? h.move(__UP__).follow() : h.move(__DOWN__).follow()
+        break
+      case 's':
+        h.dir !== __UP__ ? h.move(__DOWN__).follow() : h.move(__UP__).follow()
+        break
+      case 'a':
+        h.dir !== __RIGHT__ ? h.move(__LEFT__).follow() : h.move(__RIGHT__).follow()
+        break
+      case 'd':
+          h.dir !== __LEFT__ ? h.move(__RIGHT__).follow() : h.move(__LEFT__).follow()
+        break
+      default:
+    }
+  }, 200 / level)
 })
